caa1142c47dc1449e5f2fac7372bcaa3f893043a
Mension <72541408+Gddr100x@users.noreply.github.com>
2022-12-10 19:11:34 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,11 @@
 import sys
 import os
-##from pybind11 import get_cmake_dir
-##from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup, find_packages
 
 __version__ = "0.0.1"
 
-# The main interface is through Pybind11Extension.
-# * You can add cxx_std=11/14/17, and then build_ext can be removed.
-# * You can set include_pybind11=false to add the include directory yourself,
-#   say from a submodule.
-
-#ext_modules = [
-#    Pybind11Extension("cpp_ext",
-#        [os.getcwd()+"/*.cpp"],
-#        # Example: passing in the version to the compiled code
-#        define_macros = [('VERSION_INFO', __version__)],
-#        ),
-#]
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
 
 setup(
     name = "PyMPCD_MD",
@@ -26,9 +14,8 @@ setup(
     author_email = "evenwong@stu.cdut.edu.cn",
     url = "https://github.com/Gddr100x/PyMPCD-MD",
     description = "A python-based simulation package about mpcd and md.",
-    long_description = "",
+    long_description = long_description,
     extras_require = {},
-##    ext_modules=ext_modules,
     packages = find_packages(),
     zip_safe = False,
     python_requires = ">=3.0",
