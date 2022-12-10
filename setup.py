e2901ecc5d34c3c719d74622ee42f69001a5a4e0
Mension <72541408+Gddr100x@users.noreply.github.com>
2022-12-10 21:35:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ import sys
 import os
 from setuptools import setup, find_packages
 
+__version__ = "0.0.1"
+
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
     name = "mpcmd",
     author = "Even Wong",
+    version = __version__,
     author_email = "evenwong@stu.cdut.edu.cn",
     url = "https://github.com/Gddr100x/PyMPCD-MD",
     description = "A python-based simulation package about mpcd and md.",
