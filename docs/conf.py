0a5ab089fd5f832d1dc7d8a7cc5db31b3468f4d9
Mension <72541408+dotmet@users.noreply.github.com>
2023-08-26 16:55:38 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+source_suffix = '.rst'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
