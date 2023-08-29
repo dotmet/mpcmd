c6b635fed4424124d2c9c648570882e72dc82faf
Mension <72541408+dotmet@users.noreply.github.com>
2023-08-29 19:21:09 +0800

    Update test_import.py

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,8 +1,8 @@
 import mpcmd
 
-assert mpcmd.__version__ == "0.0.5"
+assert mpcmd.__version__ == "0.0.6"
 
 def test_mpcmd_import():
     
     import mpcmd
-    assert mpcmd.__version__ == "0.0.5"
+    assert mpcmd.__version__ == "0.0.6"
