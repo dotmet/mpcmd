a7ed1eabdd67495dee7ccc54412cf35fe8e72a11
Mension <72541408+dotmet@users.noreply.github.com>
2023-08-29 19:03:23 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 import os
 from setuptools import setup, find_packages
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
